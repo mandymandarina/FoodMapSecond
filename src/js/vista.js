6d2881eb29c83a3ea6ca954f7d9b32663093e049
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-08-16 13:00:54 -0300

    cambios en la api

---
+++ src/js/vista.js
@@ -0,0 +1,24 @@
+const containerTitle = document.getElementById('title');
+const containerDireccion = document.getElementById('direccion');
+const containerHorario = document.getElementById('horario');
+
+inputGroupSelect02.addEventListener('change', () => {
+  const ubication = `https://places.cit.api.here.com/places/v1/discover/search?app_id=7ruK6i4yX7FUmmecHtyw&app_code=Kfxkypo1SzkZogn5nO4eag&at=${HEREHQcoordinates.lat},${HEREHQcoordinates.lng}&q=${inputGroupSelect02.value}&pretty`
+  fetch(ubication)
+  .then(response => response.json())
+  .then(explorer => {
+    console.log(explorer);
+    explorer.results.items.forEach((item)=>{
+      let explorer = {
+      name: item.title,
+      direccion: item.vicinity
+    }
+    });
+    renderInfo(explorer);
+  });
+});
+
+const renderInfo = (explorer) => {
+  containerTitle.innerHTML += `<p>${JSON.stringify(explorer.results.items[0].title)}
+  ${JSON.stringify(explorer.results.items[0].vicinity)}</p>`;
+}
