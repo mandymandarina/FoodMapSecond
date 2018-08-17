c602b255d042491b83caf2272b54b8d792b0c174
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-08-17 17:50:12 -0300

    cambios

--- src/js/vista.js
+++ src/js/vista.js
@@ -6,19 +6,19 @@ btnData.addEventListener('click', () => {
   const ubication = `https://places.cit.api.here.com/places/v1/discover/search?app_id=7ruK6i4yX7FUmmecHtyw&app_code=Kfxkypo1SzkZogn5nO4eag&at=${HEREHQcoordinates.lat},${HEREHQcoordinates.lng}&q=${inputGroupSelect02.value}&pretty`
   fetch(ubication)
   .then(response => response.json())
-  .then(explorer => {
-    console.log(explorer);
-    explorer.results.items.forEach((item)=>{
-      let explorer = {
+  .then(data => {
+    console.log(data);
+    data.results.items.forEach((item)=>{
+      let data = {
       name: item.title,
       direccion: item.vicinity
     }
     });
-    renderInfo(explorer);
+    renderInfo(data);
   });
 });
 
-const renderInfo = (explorer) => {
-  contenedorData.innerHTML += `<p class="descriptionFodd"> Nombre Local: ${JSON.stringify(explorer.results.items[0].title)}
-  Dirección: ${JSON.stringify(explorer.results.items[0].vicinity)} Calificación: ${JSON.stringify(explorer.results.items[0].averageRating)}</p>`;
+const renderInfo = (data) => {
+  contenedorData.innerHTML += `<p class="descriptionFodd"> Nombre Local: ${JSON.stringify(data.results.items[0].title)}
+  Dirección: ${JSON.stringify(data.results.items[0].vicinity)} Calificación: ${JSON.stringify(data.results.items[0].averageRating)}</p>`;
 }
